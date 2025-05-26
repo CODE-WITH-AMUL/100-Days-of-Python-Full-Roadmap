596066e4034be3731cab15152e2b6946086328a1
AMUL SHARMA <140779732+CODE-WITH-AMUL@users.noreply.github.com>
2025-05-26 19:52:06 +0545

    Python basic

---
+++ Python Basic/python.py
@@ -0,0 +1,24 @@
+print("Hello World!")
+# what does print function doest 
+# > Outputs 
+print("Hay we are learning python fram stratch hear")
+
+# single comment 
+''' 
+
+sdkfj sudf sjdfiuqf iuwerfh9if938ogf
+
+'''
+
+# Variable and Data types
+
+name = "code with amul" #string 
+is_student = True # bool (Boolean)
+discription = "I am teaching python"
+age = 22 # int (insteger)
+
+#print(name , is_student , discription , age)
+print(name)
+print(is_student)
+print(discription)
+print(age)

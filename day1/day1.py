cee357e0b4030b9ff159bd2c321cd66926bf9fd8
AMUL SHARMA <140779732+CODE-WITH-AMUL@users.noreply.github.com>
2025-07-12 23:14:40 +0545

    #The Day1 of learing python

---
+++ day1/day1.py
@@ -0,0 +1,23 @@
+#integes and floats 
+a = 1
+b = 3.4
+
+# string 
+name = "Amul Sharma"
+greeting = name + " " + "is a good boy"
+print(greeting)
+
+
+#list and tuples
+a = ['a' , 'b', 'c']
+numbers = [1,2,3,4]
+coordinats = (23, 45)
+
+#dictonaries 
+name = {"persom" : "Amul Sharma" , "age" : 21}
+
+
+#Booleans 
+Male = True
+Female = False
+
---
+++ day1/task.py
@@ -0,0 +1,22 @@
+# wap to print the message in python
+print("Hello my name is Amul sharma")
+
+# pratice creating and manipulating varibles of diffrent data types
+
+int_var = 12
+float_var = 3.14
+string_var = "Amul Sharma"
+list_var = [1 , 2 , 3]
+tuple_var = (1 , 2, 3)
+dict_var = {"name" : "Amul Sharmna" , "age" : 20}
+bool_var = True or False
+
+# Manuplating the data types 
+
+print("The integer varible is: " , int_var)
+print("The flaot value is : " , float_var)
+print("The string value is : " , string_var + "" + "welcome hear !")
+print("The updated list value is : " , list_var.append(5))
+print('The value of tuple is: ' , tuple_var)
+print("The Dictonary of the value is :" , dict_var['name'])
+print("The bool value is : " , bool_var)
